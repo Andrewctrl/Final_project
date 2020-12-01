b320dbc21968e8c70f156335f4a238fadf06b314
alexanduh <alexander.j.peng@gmail.com>
2020-11-30 17:19:30 -0800

    Create test copy.py

---
+++ test copy.py
@@ -0,0 +1,23 @@
+#Choose your own Adventure
+print ("You wake up to a chilly Saturday morning shivering in your blankets, the alarm starts ringing. Do you get out of bed or stay?")
+answer = input("Yes or No? ")
+if answer_1 == "yes":
+    print("You get out of bed and prepare a breakfast of instant noodles with the carton of milk you saved from yesterday’s school lunch")
+    answer = input("You also get a text from mother, do you look?")
+    if(answer == "yes"):
+        print("blah") #result of choice
+        answer = input("") #subsequent question
+        if:
+
+else:
+    print("You continue to sleep and drift off into your dreams")
+    #while  == "yes":
+        #print("Hello, Son/Daughter I hoped you enjoyed your breakfeast do you mind visiting me at the hospitial and taking over the ice cream cart business for today, do you go and take over or not?")
+        #answer_2 = input("Yes or No?")
+    
+        #if answer_2 == "yes":
+            #print("Sure, mom I'll see if can come by today and take over the business")
+        #else:
+            #print("You ignore the text that your mom has sent, and spend the rest of the morning studying for Monday’s test and playing on your phone.")
+
+
