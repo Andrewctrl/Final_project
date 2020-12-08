7c29e3cc44c150a1680f7c9d9bba114b58f90cd4
Andrewctrl <71676283+Andrewctrl@users.noreply.github.com>
2020-12-07 17:24:15 -0800

    Debugged a bit

--- alpha.py
+++ alpha.py
@@ -4,7 +4,7 @@ import Visting_Mom
 def chose_yes():
     print("You ignore the text that your mom has sent, and spend the rest of the morning studying for Monday’s test and playing Clash Royale on your phone. As lunch edges closer, your mom has become increasingly worried and texts you again. This time she says, “Dolores, I’m worried about you. My terminal pancreatic cancer has metastasized and in a few days, I’ll no longer be with you to guide you. I’m sorry that I could not provide you a childhood you so longed for.” ")
     answer = input("Do you respond with, “ I want to have an equal shot at an education just like everyone else does in my school”, or do you say, “No need to be sorry mom, I’m coming to visit you right now.” Yes for equal shot for education No for visting mom")
-    if(answer == "yes"): # eqaul shot for edu 
+    if(answer == "Yes"): # eqaul shot for edu 
         equal_shot.chosen()
-    if(answer == "no"): # visting mom 
+    if(answer == "No"): # visting mom 
         Visting_Mom.choice() 
