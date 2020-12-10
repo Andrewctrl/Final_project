b5d4c17173f0ce1d9dd4d21efbb80ee14f5327df
Andrewctrl <71676283+Andrewctrl@users.noreply.github.com>
2020-12-09 18:06:45 -0800

    asdqwe

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 print("Hello World")
 print("andrew one big clown")
 from PIL import Image
-# im = Image.open(r"C:\Users\andrewli\Documents\servo.png")
+#im = Image.open(r"C:\Users\andrewli\Documents\servo.png")
 
 im = Image.open("servo.png")
 
